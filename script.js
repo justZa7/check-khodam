b739a7f8c75ae5bf2720929a1e26ffa4afc6aa4d
justZa7 <154692649+justZa7@users.noreply.github.com>
2024-07-25 08:01:38 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+document.getElementById('khodamForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    let name = document.getElementById('name').value;
+    let result = document.getElementById('result');
+    
+    let khodams = [
+        'Kucing mewing', 
+        'Slamet kopling', 
+        'Izul prasmanan', 
+        'Yoga warteg', 
+        'Ubi jalar',
+        'Lato lato mojokerto',
+        'Supra 2000cc braktaktak',
+        'Puding cokelat hambali',
+        'Laba laba sunda',
+        'Anggrek mekar pontianak',
+        'Kapal karam',
+        'Zeus',
+        'Suki liar',
+        'Kicau mania',
+        'Juna bin junaide',
+        'Gilang ojol',
+        'Kak gem',
+        'Kereta argo ngawi jesgejes',
+        'Pokemon bogor',    
+    ];
+    let randomIndex = Math.floor(Math.random() * khodams.length);
+    let khodam = khodams[randomIndex];
+    
+    result.textContent = `${khodam}`;
+});
